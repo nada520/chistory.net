8f3aef4c361d67f801eb7e9a1b2a3fc435971757
nada520 <49468592+nada520@users.noreply.github.com>
2025-12-29 12:28:05 +0800

    Add files via upload

---
+++ diana/WDDATA.js
@@ -0,0 +1,94 @@
+window.MY_WORD_DATABASE = {
+  FRUIT_DATA: {
+    title: "水果 (Fruit)",
+    categories: [
+      {
+        name: "柑橘类 (Citrus)",
+        items: [
+          { en: "orange", zh: "橙" },
+          { en: "lemon", zh: "柠檬" },
+          { en: "lime", zh: "青柠" },
+          { en: "grapefruit", zh: "柚子/葡萄柚" },
+          { en: "kumquat", zh: "金桔" },
+          { en: "mandarine", zh: "桔子" },
+          { en: "pamelo", zh: "柚子" }
+        ]
+      },
+      {
+        name: "浆果类 (Berries)",
+        items: [
+          { en: "mulberry", zh: "桑葚" },
+          { en: "strawberry", zh: "草莓" },
+          { en: "blueberry", zh: "蓝莓" },
+          { en: "blackberry", zh: "黑莓" },
+          { en: "cranberry", zh: "蔓越莓" },
+          { en: "raspberry", zh: "覆盆子/树莓" },
+          { en: "gooseberry", zh: "醋栗" }
+        ]
+      },
+      {
+        name: "热带与亚热带",
+        items: [
+          { en: "banana", zh: "香蕉" },
+          { en: "durian", zh: "榴莲" },
+          { en: "jackfruit", zh: "菠萝蜜" },
+          { en: "roseapple", zh: "莲雾" },
+          { en: "pineapple", zh: "菠萝" },
+          { en: "dragonfruit", zh: "火龙果" },
+          { en: "lychee", zh: "荔枝" },
+          { en: "longan", zh: "龙眼" },
+          { en: "coconut", zh: "椰子" },
+          { en: "mango", zh: "芒果" },
+          { en: "papaya", zh: "木瓜" },
+          { en: "kiwi fruit", zh: "猕桃/奇异果" },
+          { en: "passionfruit", zh: "百香果/激情果" },
+          { en: "avocado", zh: "牛油果" }
+        ]
+      },
+      {
+        name: "核果与仁果",
+        items: [
+          { en: "apple", zh: "苹果" },
+          { en: "pear", zh: "梨" },
+          { en: "loquat", zh: "枇杷" },
+          { en: "peach", zh: "桃子" },
+          { en: "apricot", zh: "杏" },
+          { en: "plum", zh: "李子" },
+          { en: "prune", zh: "西梅" },
+          { en: "cherry", zh: "樱桃" },
+          { en: "nectarine", zh: "油桃" },
+          { en: "persimmon", zh: "柿子" },
+          { en: "date", zh: "枣" },
+          { en: "pomegranate", zh: "石榴" }
+        ]
+      }
+    ]
+  },
+  VEGETABLE_DATA: {
+    title: "蔬菜 (Vegetable)",
+    categories: [
+      {
+        name: "瓜果与茄类",
+        items: [
+          { en: "tomato", zh: "番茄" },
+          { en: "cucumber", zh: "黄瓜" },
+          { en: "eggplant", zh: "茄子" },
+          { en: "pumpkin", zh: "南瓜" },
+          { en: "zucchini", zh: "西葫芦" }
+        ]
+      },
+      {
+        name: "根茎类",
+        items: [
+          { en: "potato", zh: "土豆" },
+          { en: "carrot", zh: "胡萝卜" },
+          { en: "radish", zh: "小萝卜" },
+          { en: "turnip", zh: "芜菁 大萝卜" },
+          { en: "beetroot", zh: "红菜根" },
+          { en: "lotus root", zh: "藕" },
+          { en: "bambooshoot", zh: "竹笋" }
+        ]
+      }
+    ]
+  }
+};
